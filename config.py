38bb14831d387b776d2564ba544cef4cf4bb674f
elfish <32353376+elfish0@users.noreply.github.com>
2018-11-09 20:25:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,16 @@ class Desired():
     desired_caps['automationName'] = 'Appium'
     desired_caps['deviceName'] = 'Android Emulator'
     desired_caps['platformName'] = 'android'
-    desired_caps['platformVersion'] = '8.0'
+    desired_caps['platformVersion'] = '8.0'    
+    desired_caps['appPackage'] = 'com.abc.def'
+    desired_caps['appActivity'] = '.ui.abc.LoginActivity'
     desired_caps['noReset'] = True        # 是否重新安装app  True则不重新安装
-    desired_caps['appPackage'] = 'com.smartspyeye.lyclound'
-    desired_caps['appActivity'] = '.ui.aty.LoginActivity'
+    
     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
     sleep(5)
 
 
 class Account():
-    user = 'dengliyu'
-    psw = 'jiaqi706'
+    user = 'dly'
+    psw = 'you123'
 
