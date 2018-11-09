422a24c6009b08023799ab5ae07e2da428559c2e
elfish <32353376+elfish0@users.noreply.github.com>
2018-11-09 20:18:13 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,8 +1,8 @@
 from code.config import Desired, Account
 from code.common import editText
 
-user_id = 'com.smartspyeye.lyclound:id/det_user'      # 账号输入框的resource-id
-psw_id = 'com.smartspyeye.lyclound:id/det_psd'        # 密码输入框的resource-id
+user_id = 'com.abc.def:id/det_user'      # 账号输入框的resource-id
+psw_id = 'com.abc.def:id/det_psd'        # 密码输入框的resource-id
 user = Account().user
 psw = Account().psw
 
@@ -10,7 +10,7 @@ def login(user_id,psw_id,user,psw):
     driver = Desired().driver
     editText(user_id, user)
     editText(psw_id, psw)
-    driver.find_element_by_id('com.smartspyeye.lyclound:id/btn_login').click()
+    driver.find_element_by_id('com.abc.def:id/btn_login').click()
 
 if __name__ == "__main__":
     login(user_id, psw_id, user, psw)
